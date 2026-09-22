6e27de37eff239c0c9e803b65519f51d321ac663
Sara-Luongo <saraluongo202002@gmail.com>
2026-09-22 12:53:52 +0200

    creato array di post creato test che fallisce e creata funzione eseguito test andato a buon fine ed eseguoto refactor per lo snack n7

--- esercizi-snack.js
+++ esercizi-snack.js
@@ -1,3 +1,38 @@
+
+const posts = [
+    {
+        id: 1,
+        title: "Come iniziare a programmare da zero",
+        slug: "come-iniziare-a-programmare-da-zero"
+    },
+    {
+        id: 2,
+        title: "Le basi di JavaScript",
+        slug: "le-basi-di-javascript"
+    },
+    {
+        id: 3,
+        title: "Guida alla creazione di un sito web",
+        slug: "guida-alla-creazione-di-un-sito-web"
+    },
+    {
+        id: 4,
+        title: "Cos'è un'API e come funziona",
+        slug: "cose-un-api-e-come-funziona"
+    },
+    {
+        id: 5,
+        title: "Introduzione a React",
+        slug: "introduzione-a-react"
+    },
+    {
+        id: 6,
+        title: "Come migliorare le proprie competenze di sviluppo",
+        slug: "come-migliorare-le-proprie-competenze-di-sviluppo"
+    }
+];
+
+
 function getInitials(stringa) {
     const nomeCompleto = [];
     const nomeInArray = stringa.split(" ")
@@ -12,6 +47,14 @@ function getInitials(stringa) {
 };
 
 function createSlug(string) {
+    const symbols = "-?=)(/&%$£!"
+    const stringAsArr = [...string]
+    if (string === '' || stringAsArr.some(s => {
+        return [...symbols].includes(s)
+    })) {
+        throw new Error("la stringa è vuota o non valida");
+
+    }
     const sluggedString = string.replaceAll(" ", "-")
     return sluggedString.toLowerCase()
 };
@@ -24,8 +67,26 @@ function avarage(arrayNum) {
     return somma / arrayNum.length
 }
 
+function isPalindrome(string) {
+    for (let i = 0; i < string.length; i++) {
+        const lastChar = string.length - 1 - i
+        if (string[i] !== string[lastChar]) {
+            return false
+        }
+    }
+
+    return true
+}
+function findPostById(post, id) {
+    return post.find(p => {
+        return p.id === id
+    })
+}
+
 module.exports = {
     getInitials,
     createSlug,
-    avarage
+    avarage,
+    isPalindrome,
+    findPostById
 }
--- esercizi-snack.test.js
+++ esercizi-snack.test.js
@@ -1,15 +1,17 @@
-const { getInitials, createSlug, avarage } = require("./esercizi-snack.js");
+const { getInitials, createSlug, avarage, isPalindrome, findPostById } = require("./esercizi-snack.js");
 
-// /* snack 1 */
-// test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
-//     expect(getInitials('Sara Luongo')).toBe('SL')
-// });
+
+
+/* snack 1 */
+test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
+    expect(getInitials('Sara Luongo')).toBe('SL')
+});
 
 
 // /* snack 2 */
-// test('La funzione createSlug restituisce una stringa in lowercase.', () => {
-//     expect(createSlug('Ciao A Tutti')).toBe('ciao a tutti')
-// });
+test('La funzione createSlug restituisce una stringa in lowercase.', () => {
+    expect(createSlug('Ciao A Tutti')).toBe('ciao a tutti')
+});
 
 
 /* snack 3 */
@@ -20,4 +22,28 @@ test('La funzione average calcola la media aritmetica di un array di numeri.', (
 /* snack 4 */
 test('La funzione createSlug sostituisce gli spazi con -.', () => {
     expect(createSlug('Ciao Tutto Bene')).toBe('ciao-tutto-bene')
+});
+
+
+/* snack 5 */
+test('La funzione isPalindrome verifica se una stringa è un palindromo.', () => {
+    expect(isPalindrome('osso')).toBeTruthy()
+});
+
+/* snack 6 */
+test('La funzione createSlug lancia un errore se il titolo è vuoto o non valido.', () => {
+    expect(() => createSlug('')).toThrow()
+});
+test('La funzione createSlug lancia un errore se il titolo è vuoto o non valido.', () => {
+    expect(() => createSlug('ciao??')).toThrow()
+});
+
+/* snack 7 */
+
+test('La funzione findPostById restituisce il post corretto dato l’array di post e l’id', () => {
+    expect(findPostById(posts, 2)).toEqual({
+        id: 2,
+        title: "Le basi di JavaScript",
+        slug: "le-basi-di-javascript"
+    },)
 });
